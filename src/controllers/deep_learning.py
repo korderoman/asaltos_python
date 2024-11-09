9e8f9156c48bd8fbfd9b9362cfa6170798e74f0b
HenryMedina <hmedinar@proveedor.intercorp.com.pe>
2024-11-08 22:52:33 -0500

    cambios hacia mp4

--- src/controllers/deep_learning.py
+++ src/controllers/deep_learning.py
@@ -31,13 +31,14 @@ class DeepLearningController:
             # el pipeline debe de guardar el video dentro de data y agregar un sufijo
             # al nombre del video que se llame learned
             input_path_mp4 = self.get_input_local_path_mp4(id_video=id_video)
-            output_path_mp4 = self.get_output_local_path_mp4(id_video=id_video)
+            #output_path_mp4 = self.get_output_local_path_mp4(id_video=id_video)
             input_path_avi=self.pass_from_mp4_to_avi(input_path_mp4=input_path_mp4, id_video=id_video)
             output_path_avi=self.pass_to_output_path_avi_processed(output_path=input_path_avi)
+            output_path_mp4=self.pass_from_avi_to_mp4(input_path_avi=output_path_avi,id_video=id_video)
             #print("data",output_path_avi, input_path_avi)
             prediction=self.pipelineController.predecir_video(input_path_avi, output_path_avi)
             return {"success": True, "message": "El vídeo ha sido procesado con éxito",
-                    "extras": {"local_path": output_path_avi, "name_file": f"{id_video}_learned.avi","has_violence":self.has_violence(prediction)}}
+                    "extras": {"local_path": output_path_mp4, "name_file": f"{id_video}_learned.mp4","has_violence":self.has_violence(prediction)}}
         return {"success": False, "message": "El video no existe o no ha sido posible procesarlo"}
 
     def pass_to_output_path_avi_processed(self, output_path):
@@ -50,6 +51,32 @@ class DeepLearningController:
         return os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "data", id_video + "_learned.mp4"))
     def get_output_local_path_avi(self, id_video):
         return os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "data", id_video + ".avi"))
+
+    def pass_from_avi_to_mp4(self, input_path_avi, id_video):
+        print("buscando el archivo para el pase de avi a mp4")
+        ouput_filename_mp4=f"{id_video}_learned.avi"
+        output_filepath_mp4 = self.get_output_local_path_mp4(id_video=id_video)
+        ffmpeg_path = "ffmpeg"
+        if platform.system() == "Windows":
+            ffmpeg_path = r"C:\ffmpeg\bin\ffmpeg.exe"
+            # Comando de conversión con FFmpeg
+        ffmpeg_command = [
+            ffmpeg_path, "-y", "-i", input_path_avi, "-c:v", "libx264", "-crf", "23", "-preset", "medium", "-c:a", "aac",
+            "-b:a", "192k", output_filepath_mp4
+        ]
+        # Ruta completa al ejecutable de FFmpeg
+        try:
+            # Ejecutar el comando de FFmpeg
+            subprocess.run(ffmpeg_command, check=True)
+            print("Conversión completada con éxito.")
+            return output_filepath_mp4
+        except subprocess.CalledProcessError as e:
+            print(f"Error durante la conversión: {e}")
+        except FileNotFoundError:
+            print("FFmpeg no está instalado o no se encuentra en el PATH.")
+        except Exception as e:
+            print(f"Ha ocurrido un error inesperado: {e}")
+
     def pass_from_mp4_to_avi(self, input_path_mp4, id_video):
         print("buscando el archivo para el pase de mp4 a avi: ",input_path_mp4 )
         output_filename_avi=f"{id_video}_learned.avi"
@@ -61,10 +88,6 @@ class DeepLearningController:
            ffmpeg_path, '-i', input_path_mp4, '-c:v', 'libxvid', '-qscale:v', '5','-c:a', 'libmp3lame', '-qscale:a', '4','-y',
           output_filepath_avi
         ]
-        #ffmpeg_command = [
-         #   ffmpeg_path, "-y", "-i", input_path_mp4, "-c:v", "libxvid", "-b:v", "500k",
-         #   "-c:a", "libmp3lame", "-b:a", "128k", output_filepath_avi
-        #]
         # Ruta completa al ejecutable de FFmpeg
         try:
             # Ejecutar el comando de FFmpeg
